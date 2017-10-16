911003f843535fbe9da1e50b9271a4d02198b593
Mike <mikecokina@gmail.com>
2017-10-16 22:35:39 +0200

    System and BinarySystem

--- engine/system.py
+++ engine/system.py
@@ -1,3 +1,22 @@
 
 class System(object):
-    pass
+    """
+    Abstract class defined System
+    """
+
+    ID = 1
+
+    def __init__(self, name=None):
+        if name is None:
+            self._name = str(System.ID)
+            System.ID += 1
+        else:
+            self._name = str(name)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
