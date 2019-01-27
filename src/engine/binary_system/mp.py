2991b9e70ff858ee1291eb6d5c25544b1ec3a90b
mike <mikecokina@gmail.com>
2019-01-27 20:02:43 +0100

    used generator expr instead of explicit list

--- src/engine/binary_system/mp.py
+++ src/engine/binary_system/mp.py
@@ -5,8 +5,8 @@ from engine.binary_system import model
 
 
 def prepare_get_surface_points_args(preacalc_vals_args, mass_ratio, surface_potential):
-    return [(idx, mass_ratio, surface_potential)
-            + preacalc_vals_arg for idx, preacalc_vals_arg in enumerate(preacalc_vals_args)]
+    return ((idx, mass_ratio, surface_potential) +
+            preacalc_vals_arg for idx, preacalc_vals_arg in enumerate(preacalc_vals_args))
 
 
 def get_surface_points_worker(*args):
