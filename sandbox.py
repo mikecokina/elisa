46edaeabf45ea05f1bc0b4bcc84f66d9e5e77777
mike <mikecokina@gmail.com>
2019-05-22 23:07:10 +0200

    sandbox - looking for matrix solution computaion of line intersections

--- sandbox.py
+++ sandbox.py
@@ -1,29 +1,44 @@
 import numpy as np
-from elisa.engine import utils, const
 
 
-look_in = np.array(const.CK_GRAVITY_LIST_ATM)
+face = np.array([
+        [0.0, 0.0],
+        [1.0, 0.0],
+        [0.6, 0.7],
+        [-0.2, 0.3]
+    ])
 
-look_for = np.array([0.0, 0.1, 0.3, 4.0, 5.0])
+polygon = np.array(
+    [
+        [0.4, 0.2],
+        [0.4, -0.5],
+        [1.5, -0.5],
+        [1.5, 0.3],
+        [0.75, 0.3]
+    ]
+)
 
-# v = utils.find_nearest_value_as_matrix(look_in, look_for)
-v = utils.find_surrounded_as_matrix(look_in, look_for)
+face_edges = np.zeros((len(face), 2, 2))
+face_edges[:, 0, :] = face
+face_edges[:, 1, :] = np.roll(face, axis=0, shift=1)
 
-print(const.CK_GRAVITY_LIST_ATM)
-print(v)
+polygon_edges = np.zeros((len(polygon), 2, 2))
+polygon_edges[:, 0, :] = polygon
+polygon_edges[:, 1, :] = np.roll(polygon, axis=0, shift=1)
 
-# x = np.array([0.5, 0.1, 0.2, 0.3, 0.5])
-# z = np.zeros(len(x))
-# y = np.array([0.5, 0.2])
-# duplicate = np.isin(x, y)
-#
-#
-#
-# print(duplicate)
-#
+dp1 = face_edges[:, 0] - face_edges[:, 1]
+dp2 = polygon_edges[:, 0] - polygon_edges[:, 1]
 
 
+m1, n1 = dp1.shape
+m2, n2 = dp2.shape
 
+matrix = np.zeros((m1, m2, n1+n2), dtype=dp1.dtype)
+matrix[:, :, :n1] = dp1[:, None, :]
+matrix[:, :, n1:] = dp2
+matrix = matrix.reshape(-1, 2, 2)
+
+determinatns = matrix[:, 0, 0] * matrix[:, 1, 1] - matrix[:, 0, 1] * matrix[:, 1, 0]
 
 
 
