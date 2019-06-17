dfab2b9cfcc3edac17cdc93b94fe8cbc689c985e
mike <mikecokina@gmail.com>
2019-06-17 18:16:16 +0200

    ld module: small enhacements and fixes

--- src/elisa/engine/ld.py
+++ src/elisa/engine/ld.py
@@ -3,14 +3,13 @@ import logging
 import numpy as np
 import pandas as pd
 
+from scipy import interpolate
 from elisa.conf import config
 from elisa.engine import utils, const
-from scipy import interpolate
-
 from elisa.engine.utils import is_empty
 
 config.set_up_logging()
-logger = logging.getLogger("ld")
+logger = logging.getLogger("limb-darkening-module")
 
 
 def get_metallicity_from_ld_table_filename(filename):
@@ -20,6 +19,7 @@ def get_metallicity_from_ld_table_filename(filename):
     :param filename: str
     :return: float
     """
+    filename = os.path.basename(filename)
     m = str(filename).split(".")[-2]
     sign = 1 if str(m).startswith("p") else -1
     value = float(m[1:]) / 10.0
@@ -32,7 +32,7 @@ def get_van_hamme_ld_table_filename(passband, metallicity, law=None):
 
     :param passband: str
     :param metallicity: str
-    :param law: str
+    :param law: str; limb darkening law (`linear`, `cosine`, `logarithmic`, `square_root`)
     :return: str
     """
     law = law if not is_empty(law) else config.LIMB_DARKENING_LAW
@@ -45,7 +45,7 @@ def get_van_hamme_ld_table(passband, metallicity, law=None):
 
     :param passband: str
     :param metallicity: str
-    :param law: str
+    :param law: str; in not specified, default law specified in `elisa.conf.config` is used
     :return: pandas.DataFrame
     """
     law = law if not is_empty(law) else config.LIMB_DARKENING_LAW
@@ -69,17 +69,18 @@ def get_van_hamme_ld_table_by_name(fname):
     return pd.read_csv(path)
 
 
-def get_relevant_ld_tables(passband, metallicity):
+def get_relevant_ld_tables(passband, metallicity, law=None):
     """
     Get filename of van hamme tables for surrounded metallicities and given passband.
 
+    :param law: str; limb darkening law (`linear`, `cosine`, `logarithmic`, `square_root`)
     :param passband: str
     :param metallicity: str
     :return: list
     """
     # todo: make better decision which values should be used
     surrounded = utils.find_surrounded(const.VAN_HAMME_METALLICITY_LIST_LD, metallicity)
-    files = [get_van_hamme_ld_table_filename(passband, m) for m in surrounded]
+    files = [get_van_hamme_ld_table_filename(passband, m, law) for m in surrounded]
     return files
 
 
@@ -91,16 +92,16 @@ def interpolate_on_ld_grid(temperature, log_g, metallicity, passband, author=Non
     :param temperature: Iterable float
     :param log_g: Iterable float
     :param metallicity: float
-    :param author: str
+    :param author: str; (not implemented)
     :return: pandas.DataFrame
     """
     if isinstance(passband, dict):
         passband = passband.keys()
 
     results = dict()
-    logger.debug('interpolating ld coefficients')
+    logger.debug('interpolating limb darkening coefficients')
     for band in passband:
-        relevant_tables = get_relevant_ld_tables(passband=band, metallicity=metallicity)
+        relevant_tables = get_relevant_ld_tables(passband=band, metallicity=metallicity, law=config.LIMB_DARKENING_LAW)
         csv_columns = config.LD_LAW_COLS_ORDER[config.LIMB_DARKENING_LAW]
         all_columns = csv_columns
         df = pd.DataFrame(columns=all_columns)
@@ -109,6 +110,7 @@ def interpolate_on_ld_grid(temperature, log_g, metallicity, passband, author=Non
             _df = get_van_hamme_ld_table_by_name(table)[csv_columns]
             df = df.append(_df)
 
+        df = df.drop_duplicates()
         xyz_domain = np.array([np.array(val) for val in df[config.LD_DOMAIN_COLS].to_records(index=False)]).tolist()
         xyz_values = df[config.LD_LAW_CFS_COLUMNS[config.LIMB_DARKENING_LAW]].to_records(index=False).tolist()
 
@@ -127,11 +129,11 @@ def interpolate_on_ld_grid(temperature, log_g, metallicity, passband, author=Non
 
         for col, vals in zip(config.LD_LAW_CFS_COLUMNS[config.LIMB_DARKENING_LAW], uvw_values.T):
             if np.isin(np.nan, vals):
-                raise ValueError("Limb darkening interpolation lead to np.nan/None value. "
-                                 "It might be caused by definiton of inphysical object on input.")
+                raise ValueError("Limb darkening interpolation lead to np.nan/None value.\n"
+                                 "It might be caused by definition of unphysical object on input.")
             result_df[col] = vals
         results[band] = result_df
-    logger.debug('ld coefficients interpolation finished')
+    logger.debug('limb darkening coefficients interpolation finished')
     return results
 
 
@@ -141,37 +143,27 @@ def limb_darkening_factor(normal_vector=None, line_of_sight=None, coefficients=N
     """
     calculates limb darkening factor for given surface element given by radius vector and line of sight vector
 
-    :param line_of_sight: numpy.array - vector (or vectors) of line of sight (normalized to 1 !!!)
-    :param normal_vector: numpy.array - single or multiple normal vectors (normalized to 1 !!!)
-    :param coefficients: np.float in case of linear law
-                         np.array in other cases
-    :param limb_darkening_law: str -  `linear` or `cosine`, `logarithmic`, `square_root`
-    :param cos_theta: - if supplied, function will skip calculation of its own cos theta and will disregard
+    :param line_of_sight: numpy.ndarray; vector (or vectors) of line of sight (normalized to 1 !!!)
+    :param normal_vector: numpy.ndarray; single or multiple normal vectors (normalized to 1 !!!)
+    :param coefficients: numpy.ndarray
+    :param limb_darkening_law: str;  `linear` or `cosine`, `logarithmic`, `square_root`
+    :param cos_theta: numpy.ndarray; if supplied, function will skip calculation of its own cos theta and will disregard
     `normal_vector` and `line_of_sight`
-    :return:  gravity darkening factor(s), the same type/shape as theta
+    :return: numpy.ndarray; gravity darkening factors, the same type/shape as cos_theta
     """
     if normal_vector is None and cos_theta is None:
         raise ValueError('Normal vector(s) was not supplied.')
     if line_of_sight is None and cos_theta is None:
         raise ValueError('Line of sight vector(s) was not supplied.')
-
-    # if line_of_sight.ndim != 1 and normal_vector.ndim != line_of_sight.ndim:
-    #     raise ValueError('`line_of_sight` should be either one vector or ther same amount of vectors as provided in'
-    #                      ' radius vectors')
     if coefficients is None:
         raise ValueError('Limb darkening coefficients were not supplied.')
     if limb_darkening_law is None:
-        raise ValueError('Limb darkening rule was not supplied choose from: `linear` or `cosine`, `logarithmic`, '
-                         '`square_root`.')
-
-    # fixme: fix following commented code, test o raise makes no sense; coefficientSSSSS should be scallar? wtf?
-    # if limb_darkening_law in ['linear', 'cosine']:
-    #     if not np.isscalar(coefficients):
-    #         raise ValueError('Only one scalar limb darkening coefficient is required for linear cosine law. You '
-    #                          'used: {}'.format(coefficients))
-    # if limb_darkening_law in ['logarithmic', 'square_root']:
-    #     if not np.shape(coefficients) == (2,):
-    #         raise ValueError(f'Invalid number of limb darkening coefficients. Expected 2, given: {coefficients}')
+        raise ValueError('Limb darkening rule was not supplied choose from: '
+                         '`linear` or `cosine`, `logarithmic`, `square_root`.')
+    # if normal_vector is not None and line_of_sight is not None:
+    #     if line_of_sight.ndim != 1 and normal_vector.ndim != line_of_sight.ndim:
+    #         raise ValueError('A `line_of_sight` should be either one vector or ther '
+    #                          'name amount of vectors as provided in radius vectors.')
 
     if cos_theta is None:
         cos_theta = np.sum(normal_vector * line_of_sight, axis=-1)
@@ -215,7 +207,3 @@ def calculate_bolometric_limb_darkening_factor(limb_darkening_law: str = None, c
         return np.pi * (1 - coefficients[0] / 3 + 2 * coefficients[1] / 9)
     elif limb_darkening_law == 'square_root':
         return np.pi * (1 - coefficients[0] / 3 - coefficients[1] / 5)
-
-
-if __name__ == '__main__':
-    pass
