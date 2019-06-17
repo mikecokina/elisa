28f06a0732c282b925342b0928a4dc052d66040a
mike <mikecokina@gmail.com>
2019-06-17 18:10:14 +0200

    wip

---
+++ unit_test/test_ld.py
@@ -0,0 +1,130 @@
+import os
+import unittest
+import pandas as pd
+import numpy as np
+
+from numpy.testing import assert_array_equal
+from pandas.testing import assert_frame_equal
+
+from elisa.conf import config
+from elisa.engine import ld, const
+
+
+class TestLimbDarkeningModule(unittest.TestCase):
+    def test_get_metallicity_from_ld_table_filename(self):
+        filenames = ["lin.bolometric.m02.csv", "lin.bolometric.p00.csv",
+                     "lin.Generic.Bessell.B.p10.csv", os.path.join("path.path.csv", "lin.Generic.Bessell.B.p10.csv")]
+        expected = [-0.2, 0.0, 1.0, 1.0]
+        obtained = [ld.get_metallicity_from_ld_table_filename(filename) for filename in filenames]
+        assert_array_equal(expected, obtained)
+
+    def test_get_van_hamme_ld_table_filename(self):
+        expected = ["lin.Generic.Bessell.B.m01.csv",
+                    "lin.Generic.Stromgren.v.m50.csv",
+                    "lin.Kepler.p01.csv",
+                    "sqrt.Generic.Bessell.B.p10.csv"]
+        params = [
+            dict(passband="Generic.Bessell.B", metallicity=-0.1, law="linear"),
+            dict(passband="Generic.Stromgren.v", metallicity=-5, law="linear"),
+            dict(passband="Kepler", metallicity=0.1, law="linear"),
+            dict(passband="Generic.Bessell.B", metallicity=1.0, law="square_root")
+        ]
+        obtained = [ld.get_van_hamme_ld_table_filename(**param) for param in params]
+
+        assert_array_equal(expected, obtained)
+
+    @staticmethod
+    def expected_ld_tables():
+        path = os.path.join(os.path.dirname(__file__), "data", "vh93")
+        return [pd.read_csv(os.path.join(path, file)) for file in os.listdir(path)]
+
+    def test_get_van_hamme_ld_table(self):
+        config.VAN_HAMME_LD_TABLES = os.path.join(os.path.dirname(__file__), "data", "vh93")
+        params = [
+            dict(passband="Generic.Bessell.B", metallicity=-0.2, law="cosine"),
+            dict(passband="Kepler", metallicity=-4, law="logarithmic")
+        ]
+
+        expected = self.expected_ld_tables()
+        obtained = [ld.get_van_hamme_ld_table(**param) for param in params]
+
+        for e, o in zip(expected, obtained):
+            assert_frame_equal(e, o)
+
+    def test_get_van_hamme_ld_table_by_name(self):
+        config.VAN_HAMME_LD_TABLES = os.path.join(os.path.dirname(__file__), "data", "vh93")
+        filenames = ["lin.Generic.Bessell.B.m02.csv", "log.Kepler.m40.csv"]
+
+        expected = self.expected_ld_tables()
+        obtained = [ld.get_van_hamme_ld_table_by_name(file) for file in filenames]
+
+        for e, o in zip(expected, obtained):
+            assert_frame_equal(e, o)
+
+    def test_get_van_hamme_ld_table_by_name_same_as_get_van_hamme_ld_table(self):
+        params = [
+            dict(passband="Generic.Bessell.B", metallicity=-0.2, law="cosine"),
+            dict(passband="Kepler", metallicity=-4, law="logarithmic")
+        ]
+        filenames = ["lin.Generic.Bessell.B.m02.csv", "log.Kepler.m40.csv"]
+
+        obtained_by_params = [ld.get_van_hamme_ld_table(**param) for param in params]
+        obtained_by_filename = [ld.get_van_hamme_ld_table_by_name(file) for file in filenames]
+
+        for o1, o2 in zip(obtained_by_filename, obtained_by_params):
+            assert_frame_equal(o1, o2)
+
+    def test_get_relevant_ld_tables(self):
+        params = [
+            dict(metallicity=0.0, passband="Kepler", law="cosine"),
+            dict(metallicity=-0.01, passband="Generic.Bessell.B", law="linear"),
+            dict(metallicity=0.02, passband="SLOAN.SDSS.i", law="logarithmic"),
+            dict(metallicity=1.0, passband="SLOAN.SDSS.i", law="square_root")
+        ]
+        obtained = [ld.get_relevant_ld_tables(**param) for param in params]
+        expected = [
+            ['lin.Kepler.p00.csv', 'lin.Kepler.p00.csv'],
+            ['lin.Generic.Bessell.B.m01.csv', 'lin.Generic.Bessell.B.p00.csv'],
+            ['log.SLOAN.SDSS.i.p00.csv', 'log.SLOAN.SDSS.i.p01.csv'],
+            ['sqrt.SLOAN.SDSS.i.p10.csv', 'sqrt.SLOAN.SDSS.i.p10.csv']
+        ]
+        assert_array_equal(obtained, expected)
+
+    def test_interpolate_on_ld_grid(self):
+        config.VAN_HAMME_LD_TABLES = os.path.join(os.path.dirname(__file__), "data", "ld_grid")
+
+    def _raise_test_limb_darkening_factor(self, msg, **kwargs):
+        with self.assertRaises(Exception) as context:
+            ld.limb_darkening_factor(**kwargs)
+        self.assertTrue(msg in str(context.exception))
+
+    def test_limb_darkening_factor_raise(self):
+        normals = np.array([[1, 1, 1], [0.5, 0.5, 0.5], [-1., -1, 1.3]])
+        sov = const.LINE_OF_SIGHT
+
+        # :warning: order of tests depend on order of check in ld.limb_darkening_factor method!!!
+
+        # missing line_of_sight
+        msg = 'Line of sight vector(s) was not supplied.'
+        self._raise_test_limb_darkening_factor(msg, normal_vector=normals)
+
+        # missing normal_vector
+        msg = 'Normal vector(s) was not supplied.'
+        self._raise_test_limb_darkening_factor(msg, line_of_sight=sov)
+
+        # missing coefficients
+        msg = 'Limb darkening coefficients were not supplied.'
+        self._raise_test_limb_darkening_factor(msg, line_of_sight=sov, normal_vector=normals)
+
+        # missing limb_darkening_law
+        msg = 'Limb darkening rule was not supplied choose from'
+        self._raise_test_limb_darkening_factor(msg, line_of_sight=sov, normal_vector=normals, coefficients=3)
+
+    def test_limb_darkening_factor(self):
+        pass
+
+        # # invalid vectors dimensions
+        # with self.assertRaises(Exception) as context:
+        #     ld.limb_darkening_factor(normal_vector=normals,
+        #                              line_of_sight=np.array([sov, sov]))
+        # self.assertTrue('Normal vector(s) was not supplied.' in str(context.exception))
---
+++ unit_test/trash/_test_atm.py
@@ -0,0 +1,298 @@
+import os
+import unittest
+from copy import copy
+
+import numpy as np
+import pandas as pd
+from numpy.testing import assert_array_equal
+from pandas.testing import assert_frame_equal
+
+from elisa.conf import config
+from elisa.engine import atm
+from elisa.engine.atm import NaiveInterpolatedAtm, AtmDataContainer
+from elisa.engine.observer.observer import PassbandContainer
+
+config.CK04_ATM_TABLES = os.path.join(os.path.dirname(__file__), 'ck04')
+
+
+class TestNaiveInterpolation(unittest.TestCase):
+    def setUp(self):
+        self.temperatures = [5325.0, 10001, 25320.25, 4500]
+        self.logg = [3.1, 4.2, 4.5, 5.0]
+        self.metallicity = 0.1
+
+    def test_atm_files_ck04(self):
+        files = NaiveInterpolatedAtm.atm_files(temperature=self.temperatures,
+                                               log_g=self.logg,
+                                               metallicity=self.metallicity,
+                                               atlas="ck04")
+
+        expected_dir = 'ckp00'
+        expected = ['ckp00_5250_g30.csv', 'ckp00_10000_g40.csv', 'ckp00_25000_g45.csv', None,
+                    'ckp00_5500_g30.csv', 'ckp00_10250_g40.csv', 'ckp00_26000_g45.csv', 'ckp00_4500_g50.csv']
+
+        obtained = [os.path.basename(f) if f is not None else None for f in files]
+        assert_array_equal(expected, obtained)
+
+    def test_atm_files_k93(self):
+        # files = NaiveInterpolatedAtm.atm_files(temperature=self.temperatures,
+        #                                        log_g=self.logg,
+        #                                        metallicity=self.metallicity,
+        #                                        atlas="k93")
+
+        # todo: need implementation in config (uncomment and you will see)
+        pass
+
+    def test_atm_tables_ck04(self):
+        fpaths = NaiveInterpolatedAtm.atm_files(temperature=self.temperatures,
+                                                log_g=self.logg,
+                                                metallicity=self.metallicity,
+                                                atlas="ck04")
+
+        containers = atm.read_atm_tables(fpaths=fpaths)
+        self.assertTrue(len(self.temperatures * 2) == len(containers))
+        self.assertTrue(containers[3] is None)
+
+        for t, container in zip(self.temperatures, containers[:int(len(containers) / 2)]):
+            if container is not None:
+                self.assertTrue(t > container.temperature)
+
+        for t, container in zip(self.temperatures, containers[int(len(containers) / 2):]):
+            self.assertTrue(t <= container.temperature)
+
+    def test_atm_tables_ck04_multiple(self):
+        temperatures = [5325.0, 10001, 25320.25, 4500, 4500, 5325.0]
+        logg = [3.1, 4.2, 4.5, 5.0, 5.0, 3.1]
+        metallicity = 0.1
+
+        fpaths = NaiveInterpolatedAtm.atm_files(temperature=temperatures,
+                                                log_g=logg,
+                                                metallicity=metallicity,
+                                                atlas="ck04")
+        containers = atm.read_atm_tables(fpaths=fpaths)
+
+        self.assertTrue(len(temperatures * 2) == len(containers))
+        self.assertTrue(containers[3] is None)
+        self.assertTrue(containers[4] is None)
+
+        for t, container in zip(temperatures, containers[:int(len(containers) / 2)]):
+            if container is not None:
+                self.assertTrue(t > container.temperature)
+
+        for t, container in zip(temperatures, containers[int(len(containers) / 2):]):
+            self.assertTrue(t <= container.temperature)
+
+    def test_strip_atm_container_by_bandwidth(self):
+        model = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+        container = AtmDataContainer(
+            model=model,
+            temperature=3600.0,
+            log_g=4.3,
+            metallicity=0.0
+        )
+
+        striped = atm.strip_atm_container_by_bandwidth(container, left_bandwidth=2, right_bandwidth=5)
+        assert_array_equal(np.arange(1, 7, 1), striped.model[config.ATM_MODEL_DATAFRAME_WAVE])
+
+        container.model = model
+        striped = atm.strip_atm_container_by_bandwidth(container, left_bandwidth=0, right_bandwidth=5)
+        assert_array_equal(np.arange(0, 7, 1), striped.model[config.ATM_MODEL_DATAFRAME_WAVE])
+
+        container.model = model
+        striped = atm.strip_atm_container_by_bandwidth(container, left_bandwidth=0, right_bandwidth=25)
+        assert_array_equal(np.arange(0, 10, 1), striped.model[config.ATM_MODEL_DATAFRAME_WAVE])
+
+    def test_compute_interpolation_weights(self):
+        model = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+        temperature = np.array([3500.0, 5321, 10255.4])
+        top_temp = np.array([3500.0, 5500.0, 10500.0])
+        bottom_temp = np.array([None, 5250.0, 10250.0])
+
+        top_atm = [AtmDataContainer(
+            model=model,
+            temperature=t,
+            log_g=4.3,
+            metallicity=0.0,
+        ) for t in top_temp]
+        bottom_atm = [AtmDataContainer(
+            model=model,
+            temperature=t,
+            log_g=4.3,
+            metallicity=0.0
+        ) if t is not None else None for t in bottom_temp]
+
+        obtained = NaiveInterpolatedAtm.compute_interpolation_weights(temperature, top_atm, bottom_atm)
+
+        bottom_temp[0] = 0.0
+        expected = (temperature - bottom_temp) / (top_temp - bottom_temp)
+
+        assert_array_equal(obtained, expected)
+
+    def test_interpolate_return_exact(self):
+        model_3500 = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+
+        model_3500_orig = copy(model_3500)
+        temperature = np.array([3500.0])
+        atm_3500 = [
+            None,
+            AtmDataContainer(
+                model=model_3500,
+                temperature=3500.0,
+                log_g=0.0,
+                metallicity=0.0
+            )]
+
+        atm_container = NaiveInterpolatedAtm.interpolate(
+            atm_3500,
+            temperature=temperature,
+            log_g=[0.0],
+            metallicity=0.0,
+            left_bandwidth=2,
+            right_bandwidth=5
+        )[0]
+
+        obtained = atm_container.model
+        expected = model_3500_orig.iloc[np.arange(1, 7, 1)]
+        assert_frame_equal(obtained, expected.reset_index(drop=True))
+
+    def test_interpolate_return_interpolated_on_same_wave(self):
+        model_5250 = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+        model_5500 = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(1, 11, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+
+        temperature = np.array([5400])
+        atm_5xxx = [
+            AtmDataContainer(
+                model=model_5250,
+                temperature=5250,
+                log_g=0.0,
+                metallicity=0.0
+            ),
+            AtmDataContainer(
+                model=model_5500,
+                temperature=5500,
+                log_g=0.0,
+                metallicity=0.0
+            )]
+
+        atm_container = NaiveInterpolatedAtm.interpolate(
+            atm_5xxx,
+            temperature=temperature,
+            log_g=[0.0],
+            metallicity=0.0,
+            left_bandwidth=2,
+            right_bandwidth=6
+        )[0]
+
+        expected = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(1.6, 8.6, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(1, 8, 1)
+        })
+        obtained = atm_container.model
+        assert_frame_equal(obtained, expected)
+
+    def test_interpolate_return_interpolated_on_different_wave(self):
+        model_5250 = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 0.25),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 20, 0.5)
+        })
+        model_5500 = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+
+        temperature = np.array([5400])
+        atm_5xxx = [
+            AtmDataContainer(
+                model=model_5250,
+                temperature=5250,
+                log_g=0.0,
+                metallicity=0.0
+            ),
+            AtmDataContainer(
+                model=model_5500,
+                temperature=5500,
+                log_g=0.0,
+                metallicity=0.0
+            )]
+
+        atm_container = NaiveInterpolatedAtm.interpolate(
+            atm_5xxx,
+            temperature=temperature,
+            log_g=[0.0],
+            metallicity=0.0,
+            left_bandwidth=1.9,
+            right_bandwidth=6.1
+        )[0]
+
+        obtained = atm_container.model
+        expected = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: (0.8, 1.6, 2.4, 3.2, 4.0, 4.8, 5.6),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(1, 8, 1)
+        })
+
+        assert_frame_equal(obtained, expected)
+
+    def test_apply_passband(self):
+        model_3500 = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(2, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(2, 10, 1)
+        })
+
+        atm_containers = [
+            AtmDataContainer(
+                model=model_3500,
+                temperature=3500.0,
+                log_g=0.0,
+                metallicity=0.0,
+                fpath=''
+            )]
+        band_df = pd.DataFrame({
+            config.PASSBAND_DATAFRAME_THROUGHPUT: [0.1] * 8,
+            config.PASSBAND_DATAFRAME_WAVE: np.arange(2.4, 10.4, 1)
+        })
+        passband = {"passband": PassbandContainer(table=band_df)}
+
+        obtained = atm.apply_passband(atm_containers, passband)["passband"][0].model
+        expected = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: (0.0, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(2, 10, 1)
+        })
+        assert_frame_equal(expected, obtained)
+
+    def test_inplace_in_strip_atm_container_by_bandwidth(self):
+        model = pd.DataFrame({
+            config.ATM_MODEL_DATAFRAME_FLUX: np.arange(0, 10, 1),
+            config.ATM_MODEL_DATAFRAME_WAVE: np.arange(0, 10, 1)
+        })
+        container = AtmDataContainer(
+            model=model,
+            temperature=3600.0,
+            log_g=4.3,
+            metallicity=0.0
+        )
+
+        # disable inplace
+        striped = atm.strip_atm_container_by_bandwidth(container, left_bandwidth=2, right_bandwidth=5, inplace=False)
+        assert_array_equal(np.arange(1, 7, 1), striped.model[config.ATM_MODEL_DATAFRAME_WAVE])
+
+        # enable inplace and use same container as before (shouldn't be overwriten)
+        striped = atm.strip_atm_container_by_bandwidth(container, left_bandwidth=0, right_bandwidth=5, inplace=True)
+        assert_array_equal(np.arange(0, 7, 1), striped.model[config.ATM_MODEL_DATAFRAME_WAVE])
+
+        assert_array_equal(striped.model[config.ATM_MODEL_DATAFRAME_WAVE],
+                           container.model[config.ATM_MODEL_DATAFRAME_WAVE])
