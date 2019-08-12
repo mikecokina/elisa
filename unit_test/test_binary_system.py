ddcda343872abaed0ab62c547d1020f28ad2dbf1
mike <mikecokina@gmail.com>
2019-08-12 22:54:07 +0200

    additional binary sytems unittests

--- unit_test/test_binary_system.py
+++ unit_test/test_binary_system.py
@@ -3,6 +3,7 @@
 import unittest
 from copy import copy
 
+import numpy as np
 from astropy import units as u
 # from os.path import dirname
 # from os.path import join as pjoin
@@ -14,9 +15,6 @@ from numpy.testing import assert_array_equal
 from elisa import const as c
 from elisa.base.star import Star
 from elisa.binary_system.system import BinarySystem
-
-
-# from pandas.testing import assert_frame_equal
 
 
 class TestBinarySystemSetters(unittest.TestCase):
@@ -105,7 +103,7 @@ class TestBinarySystemInit(unittest.TestCase):
             {"primary_mass": 2.0, "secondary_mass": 1.0,
              "primary_surface_potential": 100.0, "secondary_surface_potential": 100.0,
              "primary_synchronicity": 1.0, "secondary_synchronicity": 1.0,
-             "argument_of_periastron": c.HALF_PI * u.rad, "gamma": 0.0, "period": 1.0,
+             "argument_of_periastron": c.HALF_PI * u.rad, "gamma": 0.0, "period": 2.0,
              "eccentricity": 0.0, "inclination": c.HALF_PI * u.rad, "primary_minimum_time": 0.0,
              "phase_shift": 0.0,
              "primary_t_eff": 5000, "secondary_t_eff": 5000,
@@ -116,7 +114,7 @@ class TestBinarySystemInit(unittest.TestCase):
             {"primary_mass": 2.0, "secondary_mass": 1.0,
              "primary_surface_potential": 100.0, "secondary_surface_potential": 80.0,
              "primary_synchronicity": 400, "secondary_synchronicity": 550,
-             "argument_of_periastron": c.HALF_PI * u.rad, "gamma": 0.0, "period": 1.0,
+             "argument_of_periastron": c.HALF_PI * u.rad, "gamma": 0.0, "period": 3.0,
              "eccentricity": 0.0, "inclination": 90.0 * u.deg, "primary_minimum_time": 0.0,
              "phase_shift": 0.0,
              "primary_t_eff": 5000, "secondary_t_eff": 5000,
@@ -180,20 +178,112 @@ class TestBinarySystemInit(unittest.TestCase):
              "primary_t_eff": 5000, "secondary_t_eff": 5000,
              "primary_gravity_darkening": 1.0, "secondary_gravity_darkening": 1.0,
              "primary_albedo": 0.6, "secondary_albedo": 0.6
-             }  # contact system
+             }  # over-contact system
         ]
 
+    def _prepare_systems(self):
+        s = list()
+        for i, combo in enumerate(self.params_combination):
+            primary = Star(mass=combo["primary_mass"], surface_potential=combo["primary_surface_potential"],
+                           synchronicity=combo["primary_synchronicity"],
+                           t_eff=combo["primary_t_eff"], gravity_darkening=combo["primary_gravity_darkening"],
+                           albedo=combo['primary_albedo'], metallicity=0.0)
+
+            secondary = Star(mass=combo["secondary_mass"], surface_potential=combo["secondary_surface_potential"],
+                             synchronicity=combo["secondary_synchronicity"],
+                             t_eff=combo["secondary_t_eff"], gravity_darkening=combo["secondary_gravity_darkening"],
+                             albedo=combo['secondary_albedo'], metallicity=0.0)
+
+            s.append(BinarySystem(primary=primary,
+                                  secondary=secondary,
+                                  argument_of_periastron=combo["argument_of_periastron"],
+                                  gamma=combo["gamma"],
+                                  period=combo["period"],
+                                  eccentricity=combo["eccentricity"],
+                                  inclination=combo["inclination"],
+                                  primary_minimum_time=combo["primary_minimum_time"],
+                                  phase_shift=combo["phase_shift"]))
+        return s
+
     def test_calculate_semi_major_axis(self):
-        pass
+        expected = [6702758048.0, 8783097736.0, 4222472978.0, 4222472978.0, 4222472978.0, 4222472978.0, 4222472978.0]
+        obtained = list()
+
+        for bs in self._prepare_systems():
+            obtained.append(np.round(bs.semi_major_axis, 0))
+        assert_array_equal(expected, obtained)
 
     def test__setup_periastron_critical_potential(self):
-        pass
+        expected_potentials = np.round(np.array([
+            [2.875844632141054, 2.875844632141054],
+            [93.717106763853593, 73.862399105365014],
+            [3.159639848886489, 2.935086409515319],
+            [4.027577786299736, 3.898140726941630],
+            [2.875844632141054, 2.875844632141054],
+            [3.159639848886489, 3.229240544834036],
+            [2.875844632141054, 2.875844632141054]
+        ]), 5)
+        obtained_potentials = list()
+        for bs in self._prepare_systems():
+            obtained_potentials.append([bs.primary.critical_surface_potential, bs.secondary.critical_surface_potential])
+        obtained_potentials = np.round(np.array(obtained_potentials), 5)
+        assert_array_equal(expected_potentials, obtained_potentials)
 
     def test__setup_morphology(self):
-        pass
+        expected = ['detached', 'detached', 'detached', 'detached', 'semi-detached', 'double-contact', 'over-contact']
+        obtained = list()
+
+        for bs in self._prepare_systems():
+            obtained.append(bs.morphology)
+        assert_array_equal(expected, obtained)
 
     def test_setup_components_radii(self):
-        pass
+        radii = ["forward_radius", "side_radius", "equatorial_radius", "backward_radius", "polar_radius"]
+        obtained = dict()
+
+        expected = {
+            "primary": {
+                "forward_radius": [0.01005, 0.01229, 0.3783, 0.26257, 0.57075, 0.53029, np.nan],
+                "side_radius": [0.01005, 0.01229, 0.35511, 0.24859, 0.43994, 0.41553, 0.48182],
+                "equatorial_radius": [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                "backward_radius": [0.01005, 0.01229, 0.36723, 0.25608, 0.46794, 0.44207, 0.52573],
+                "polar_radius": [0.01005, 0.01005, 0.33055, 0.24242, 0.41427, 0.37162, 0.44577]
+            },
+            "secondary": {
+                "forward_radius": [0.00506, 0.00763, 0.34966, 0.25693, 0.42925, 0.36743, np.nan],
+                "side_radius": [0.00506, 0.00763, 0.29464, 0.21419, 0.31288, 0.28094, 0.35376],
+                "equatorial_radius": [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                "backward_radius": [0.00506, 0.00763, 0.32018, 0.23324, 0.34537, 0.30842, 0.41825],
+                "polar_radius": [0.00506, 0.00635, 0.2798, 0.20994, 0.29977, 0.2489, 0.33306]
+            }
+        }
+
+        for component in ["primary", "secondary"]:
+            obtained[component] = dict()
+            for radius in radii:
+                obtained[component][radius] = list()
+                for i, bs in enumerate(self._prepare_systems()):
+                    value = np.round(getattr(getattr(bs, component), radius), 5)
+                    assert_array_equal([value], [expected[component][radius][i]])
+
+    def test_lagrangian_points(self):
+
+        expected_points = np.round(np.array([
+            [-0.80302796065835425, 0.57075157151852673, 1.5823807222136623],
+            [-0.80302796065835425, 0.57075157151852673, 1.5823807222136623],
+            [-0.80302796065835425, 0.57075157151852673, 1.5823807222136623],
+            [-0.7308068505479407, 0.41566688133312363, 1.4990376377419574],
+            [-0.80302796065835425, 0.57075157151852673, 1.5823807222136623],
+            [-0.80302796065835425, 0.57075157151852673, 1.5823807222136623],
+            [-0.80302796065835425, 0.57075157151852673, 1.5823807222136623]
+        ]), 5)
+
+        obtained_points = []
+        for bs in self._prepare_systems():
+            obtained_points.append(bs.lagrangian_points())
+
+        obtained_points = np.round(np.array(obtained_points), 5)
+        assert_array_equal(expected_points, obtained_points)
 
 
 class TestValidity(unittest.TestCase):
@@ -290,7 +380,7 @@ class TestValidity(unittest.TestCase):
 
         for kw in self.MANDATORY_KWARGS:
             kwargs = copy(initial_kwargs)
-            del(kwargs[kw])
+            del (kwargs[kw])
             with self.assertRaises(Exception) as context:
                 BinarySystem(**kwargs)
             self.assertTrue(f'Missing argument(s): `{kw}`' in str(context.exception))
