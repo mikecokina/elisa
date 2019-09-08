a81a94398d27cb4c8181ab71e6ee2d47ca70fc3c
mike <mikecokina@gmail.com>
2019-09-08 12:00:08 +0200

    limb darkening unittests finished

--- unittests/test_ld.py
+++ unittests/test_ld.py
@@ -1,13 +1,13 @@
 import os
 import unittest
-import pandas as pd
-import numpy as np
 
+import numpy as np
+import pandas as pd
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal
 
-from elisa.conf import config
 from elisa import const, ld
+from elisa.conf import config
 
 
 class TestLimbDarkeningModule(unittest.TestCase):
@@ -90,9 +90,39 @@ class TestLimbDarkeningModule(unittest.TestCase):
         ]
         assert_array_equal(obtained, expected)
 
-    def test_interpolate_on_ld_grid(self):
+    def test_interpolate_on_ld_grid_lin(self):
         config.VAN_HAMME_LD_TABLES = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "ld_grid")
-        raise Exception("Unfinished unittest")
+        temperature = np.array([5500, 5561, 5582, 5932])
+        log_g = np.array([4.2, 4.21, 4.223, 4.199]) - 2.0
+        metallicity = -0.1
+        passband = ["Generic.Bessell.B"]
+
+        expected = np.array([0.7732, 0.76364, 0.76032, 0.70833])
+
+        obtained = ld.interpolate_on_ld_grid(temperature, log_g, metallicity, passband)
+        obtained = np.round(obtained["Generic.Bessell.B"].xlin.values, 5)
+
+        self.assertTrue(np.all(obtained == expected))
+
+    def test_interpolate_on_ld_grid_log(self):
+        config.VAN_HAMME_LD_TABLES = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                  "data", "light_curves", "limbdarkening")
+        config.LIMB_DARKENING_LAW = "logarithmic"
+
+        temperature = np.array([5500, 5561, 5582, 5932])
+        log_g = np.array([4.2, 4.21, 4.223, 4.199]) - 2.0
+        metallicity = 0.0
+        passband = ["Generic.Bessell.V"]
+
+        expected_xlog = np.round(np.array([0.776400, 0.772272, 0.770870, 0.747420]), 5)
+        expected_ylog = np.round(np.array([0.204200, 0.211724, 0.214276, 0.251288]), 5)
+
+        obtained = ld.interpolate_on_ld_grid(temperature, log_g, metallicity, passband)
+        obtained_xlog = np.round(obtained["Generic.Bessell.V"].xlog.values, 5)
+        obtained_ylog = np.round(obtained["Generic.Bessell.V"].ylog.values, 5)
+
+        self.assertTrue(np.all(expected_ylog == obtained_ylog))
+        self.assertTrue(np.all(expected_xlog == obtained_xlog))
 
     def _raise_test_limb_darkening_factor(self, msg, **kwargs):
         with self.assertRaises(Exception) as context:
@@ -121,5 +151,31 @@ class TestLimbDarkeningModule(unittest.TestCase):
         msg = 'Limb darkening rule was not supplied choose from'
         self._raise_test_limb_darkening_factor(msg, line_of_sight=sov, normal_vector=normals, coefficients=3)
 
-    def test_limb_darkening_factor(self):
-        raise Exception("Unfinished unittest")
+    def test_limb_darkening_factor_linear(self):
+        ld_law = 'linear'
+        coefficients = np.array([[0.1], [0.2], [0.3], [0.4], [0.5], [0.6]])
+        cos_theta = np.array([[-0.23], [0.25], [0.85], [0.55], [0.33], [1.0]])
+
+        expected = np.array([0., 0.85, 0.955, 0.82, 0.665, 1.])
+        obtained = ld.limb_darkening_factor(coefficients=coefficients,
+                                            limb_darkening_law=ld_law,
+                                            cos_theta=cos_theta)
+
+        self.assertTrue(np.all(expected == np.round(obtained, 3)))
+
+    def test_limb_darkening_factor_logarithmic(self):
+        ld_law = 'logarithmic'
+        coefficients = np.array([[0.1, 0.9],
+                                 [0.2, 0.8],
+                                 [0.3, 0.7],
+                                 [0.4, 0.6],
+                                 [0.5, 0.5],
+                                 [0.2, 0.3]])
+        cos_theta = np.array([[-0.23], [0.25], [0.85], [0.55], [0.33], [1.0]])
+
+        obtained = ld.limb_darkening_factor(coefficients=coefficients,
+                                            limb_darkening_law=ld_law,
+                                            cos_theta=cos_theta)
+
+        expected = np.array([0., 1.127, 1.052, 1.017, 0.848, 1.])
+        self.assertTrue(np.all(expected == np.round(obtained, 3)))
