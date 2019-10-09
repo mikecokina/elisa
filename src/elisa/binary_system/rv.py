2df6a24ac2642ef4ecacd65cb7b187ddec7dbaae
mike <mikecokina@gmail.com>
2019-10-09 15:45:36 +0200

    radial velocity basic curves - naive implementation

--- src/elisa/binary_system/rv.py
+++ src/elisa/binary_system/rv.py
@@ -10,24 +10,63 @@ def distance_to_center_of_mass(primary_mass, secondary_mass, positions):
     :param positions: numpy.array
     :return:
     """
-    distance = np.array(positions)[:, 1]
+    distance = positions[:, 1]
     mass = primary_mass + secondary_mass
     com_from_primary = (distance * secondary_mass) / mass
     return com_from_primary, distance - com_from_primary
 
 
-def orbital_semi_major_axes():
+def orbital_semi_major_axes(r, eccentricity, true_anomaly):
     """
+    Return orbital semi major axis for given parameter.
 
-    :return:
+    :param r: float or numpy.array; distane from center of mass to object
+    :param eccentricity: float or numpy.array; orbital eccentricity
+    :param true_anomaly: float or numpy.array; true anomaly of orbital motion
+    :return: float or numpy.array
     """
-    pass
+    return r * (1.0 + eccentricity * np.cos(true_anomaly)) / (1.0 - np.power(eccentricity, 2))
+
+
+def _radial_velocity(semi_major_axis, inclination, eccentricity, argument_of_periastron, period, true_anomaly):
+    """
+    Compute radial velocity for given paramters.
+
+    :param semi_major_axis: float
+    :param inclination: float
+    :param eccentricity: float
+    :param argument_of_periastron: float
+    :param true_anomaly: float or numpy.array
+    :param period: float
+    :return: float or numpy.array
+    """
+    a = 2.0 * np.pi * semi_major_axis * np.sin(inclination)
+    b = period * np.sqrt(1.0 - np.power(eccentricity, 2))
+    c = np.cos(true_anomaly + argument_of_periastron) + (eccentricity * np.cos(argument_of_periastron))
+    return a * c / b
 
 
 def radial_velocity(self, **kwargs):
     position_method = kwargs.pop("position_method")
     phases = kwargs.pop("phases")
-    orbital_motion = position_method(input_argument=phases, return_nparray=False, calculate_from='phase')
+    orbital_motion = position_method(input_argument=phases, return_nparray=True, calculate_from='phase')
     r1, r2 = distance_to_center_of_mass(self.primary.mass, self.secondary.mass, orbital_motion)
 
-    print(r1, r2)
+    sma_primary = orbital_semi_major_axes(r1[-1], self.orbit.eccentricity, orbital_motion[:, 3][-1])
+    sma_secondary = orbital_semi_major_axes(r2[-1], self.orbit.eccentricity, orbital_motion[:, 3][-1])
+
+    # in physical units
+    sma_primary *= self.semi_major_axis
+    sma_secondary *= self.semi_major_axis
+
+    rv_primary = _radial_velocity(sma_primary, self.inclination, self.eccentricity,
+                                  self.argument_of_periastron, self.period, orbital_motion[:, 3]) * -1.0
+
+    rv_secondary = _radial_velocity(sma_secondary, self.inclination, self.eccentricity,
+                                    self.argument_of_periastron, self.period, orbital_motion[:, 3])
+
+    from matplotlib import pyplot as plt
+    plt.scatter(orbital_motion[:, 4], rv_primary, label="primary")
+    plt.scatter(orbital_motion[:, 4], rv_secondary, label="secondary")
+    plt.legend()
+    plt.show()
