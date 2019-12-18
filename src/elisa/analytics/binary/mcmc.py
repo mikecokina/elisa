2f98d6823a78eb63fad926296420f381468b81bd
mike <mikecokina@gmail.com>
2019-12-18 15:57:33 +0100

    mcmc light curves prep of example script

--- src/elisa/analytics/binary/mcmc.py
+++ src/elisa/analytics/binary/mcmc.py
@@ -172,6 +172,8 @@ class LightCurveFit(McMcFit, AbstractLightCurveDataMixin):
             self.last_normalization: Dict; normalization map used during fitting
             self.last_fname: str; filename of last stored flatten emcee `sampler` with metadata
 
+        Based on https://emcee.readthedocs.io/en/stable/.
+
         :param xs: Dict[str, Iterable[float]]; {<passband>: <phases>}
         :param ys: Dict[str, Iterable[float]]; {<passband>: <fluxes>};
         :param period: float; sytem period
@@ -251,6 +253,8 @@ class CentralRadialVelocity(McMcFit, AbstractCentralRadialVelocityDataMixin):
             self.last_sampler: emcee.EnsembleSampler
             self.last_normalization: Dict; normalization map used during fitting
             self.last_fname: str; filename of last stored flatten emcee `sampler` with metadata
+
+        Based on https://emcee.readthedocs.io/en/stable/.
 
         :param xs: Iterable[float];
         :param ys: Dict;
