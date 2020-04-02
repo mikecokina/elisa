fix in flux err calculation from magnitudes

src/elisa/analytics/binary/least_squares.py
@@ -62,10 +62,12 @@ class LightCurveFit(AbstractFit, AbstractLightCurveDataMixin, metaclass=ABCMeta)
             new_synthetic = dict()
             for fltr, curve in synthetic.items():
                 f = interpolate.interp1d(fit_xs, curve, kind='cubic')
-                new_synthetic[fltr] = f(phases)
+                new_synthetic[fltr] = f(phases[self.xs_reverser[fltr]])
             synthetic = new_synthetic
+        else:
+            synthetic = {band: val[self.xs_reverser[band]] for band, val in synthetic.items()}
 
-        residuals = np.array([np.sum(np.power(synthetic[band][self.xs_reverser[band]] - self.ys[band], 2)
+        residuals = np.array([np.sum(np.power(synthetic[band] - self.ys[band], 2)
                               / self.yerrs[band]) for band in synthetic])
 
         r2 = shared.r_squared(synthetic, self.ys)

src/elisa/analytics/binary/shared.py
@@ -60,7 +60,7 @@ def lc_r_squared(synthetic, *args, **x):
     """
     xs, ys, passband, discretization, morphology, xs_reverser, diff, interp_treshold = args
 
-    xs, kwargs = models.time_layer_resolver(xs, x)
+    xs, kwargs = models.rvt_layer_resolver(xs, **x)
     fit_xs = np.linspace(np.min(xs) - diff, np.max(xs) + diff, num=interp_treshold + 2) \
         if np.shape(xs)[0] > interp_treshold else xs
 

src/elisa/analytics/binary_fit/plot.py
@@ -209,11 +209,14 @@ class LCPlot(object):
 
         synth_phases = np.linspace(start_phase, stop_phase, number_of_points)
 
-        x0_vector, labels, fixed, constraint, observer = params.fit_data_initializer(fit_params)
-        renormalized = params.param_renormalizer(x0_vector, labels)
+        # x0_vector, labels, fixed, constraint, observer = params.fit_data_initializer(fit_params)
+        # renormalized = params.param_renormalizer(x0_vector, labels)
 
-        system_kwargs = params.prepare_kwargs(renormalized, labels, constraint, fixed)
-        # system_kwargs = params.x0_to_kwargs(system_kwargs)
+        fixed = params.x0_to_fixed_kwargs(fit_params)
+        constraint = params.x0_to_constrained_kwargs(fit_params)
+        param_vectorized, labels = params.x0_vectorize(fit_params)
+
+        system_kwargs = params.prepare_kwargs(param_vectorized, labels, constraint, fixed)
         system = models.prepare_binary(discretization=discretization, **system_kwargs)
         observer = Observer(passband=self.lc_fit.light_curves.keys(), system=system)
         synthetic_curves = models.synthetic_binary(synth_phases,

src/elisa/analytics/dataset/base.py
@@ -61,7 +61,7 @@ def convert_flux_error(data, error, unit, zero_point=None):
             raise ValueError('You supplied your data in magnitudes. Please also specify a zero point using keyword '
                              'argument `reference_magnitude`.')
         else:
-            error = np.power(10, (zero_point - data) / 2.5) * (np.power(10, error / 2.5) - 1)
+            error = np.power(10, error / 2.5) - 1
 
     return error
 