fix: adding star symmetry test to surface gravity calculation

src/elisa/binary_system/surface/gravity.py
@@ -156,7 +156,7 @@ def build_surface_gravity(system, components_distance, component="all"):
 
         points, faces = bgravity.eval_args_for_magnitude_gradient(star)
 
-        scaling_factor = (const.G * system.primary.mass / system.semi_major_axis**2)
+        scaling_factor = const.G * system.primary.mass / system.semi_major_axis**2
         g_acc_vector = scaling_factor * \
                        calculate_potential_gradient(components_distance, component, points=points,
                                                     synchronicity=synchronicity, mass_ratio=mass_ratio)
@@ -178,7 +178,8 @@ def build_surface_gravity(system, components_distance, component="all"):
                 pulsations.incorporate_gravity_perturbation(star, g_acc_vector, g_acc_vector_spot,
                                                             phase=system.position.phase)
 
-        gravity = np.mean(np.linalg.norm(g_acc_vector, axis=1)[faces], axis=1)
+        gravity = np.mean(np.linalg.norm(g_acc_vector, axis=1)[faces], axis=1) if star.symmetry_test else \
+            np.mean(np.linalg.norm(g_acc_vector, axis=1), axis=1)
         setattr(star, 'potential_gradient_magnitudes', gravity[star.face_symmetry_vector]) \
             if star.symmetry_test() else setattr(star, 'potential_gradient_magnitudes', gravity)
         setattr(star, 'log_g', np.log10(star.potential_gradient_magnitudes))

src/elisa/binary_system/system.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy
+from copy import deepcopy
 
 from scipy import optimize
 
@@ -60,8 +61,8 @@ class BinarySystem(System):
     :morphology: str; morphology of current system
     """
 
-    MANDATORY_KWARGS = ['gamma', 'inclination', 'period', 'eccentricity', 'argument_of_periastron']
-    OPTIONAL_KWARGS = ['phase_shift', 'additional_light', 'primary_minimum_time']
+    MANDATORY_KWARGS = ['inclination', 'period', 'eccentricity', 'argument_of_periastron']
+    OPTIONAL_KWARGS = ['gamma', 'phase_shift', 'additional_light', 'primary_minimum_time']
     ALL_KWARGS = MANDATORY_KWARGS + OPTIONAL_KWARGS
 
     COMPONENT_MANDATORY_KWARGS = ['mass', 't_eff', 'gravity_darkening', 'surface_potential', 'synchronicity',
@@ -96,6 +97,7 @@ class BinarySystem(System):
         self.argument_of_periastron = np.nan
         self.primary_minimum_time = 0.0
         self.phase_shift = 0.0
+        self.gamma = 0.0
 
         # set attributes and test whether all parameters were initialized
         # we already ensured that all kwargs are valid and all mandatory kwargs are present so lets set class attributes
@@ -219,15 +221,16 @@ class BinarySystem(System):
 
         :return: elisa.binary_system.system.BinarySystem
         """
+        data_cp = deepcopy(data)
         if _verify:
-            bsutils.validate_binary_json(data)
+            bsutils.validate_binary_json(data_cp)
 
-        kind_of = _kind_of or bsutils.resolve_json_kind(data)
+        kind_of = _kind_of or bsutils.resolve_json_kind(data_cp)
         if kind_of in ["community"]:
-            data = bsutils.transform_json_community_to_std(data)
+            data_cp = bsutils.transform_json_community_to_std(data_cp)
 
-        primary, secondary = Star(**data["primary"]), Star(**data["secondary"])
-        return cls(primary=primary, secondary=secondary, **data["system"])
+        primary, secondary = Star(**data_cp["primary"]), Star(**data_cp["secondary"])
+        return cls(primary=primary, secondary=secondary, **data_cp["system"])
 
     def to_json(self):
         """

src/elisa/graphic/graphics.py
@@ -814,6 +814,7 @@ def binary_lc_fit_plot(**kwargs):
     ax2 = fig.add_subplot(gs[1], sharex=ax1)
 
     for fltr, curve in kwargs['lcs'].items():
+        rasterize = np.shape(kwargs['x_data'][fltr])[0] > 300
         (dt_clr, clr) = (datapoint_clrs[fltr], datapoint_clrs[fltr]) if len(kwargs['lcs']) > 1 else ('blue', 'red')
 
         if kwargs['y_err'][fltr] is None:
@@ -824,10 +825,10 @@ def binary_lc_fit_plot(**kwargs):
                         color=dt_clr)
         else:
             ax1.errorbar(kwargs['x_data'][fltr], kwargs['y_data'][fltr], yerr=kwargs['y_err'][fltr],
-                         linestyle='none', markersize=3, label=fltr + ' observed', color=dt_clr)
+                         linestyle='none', markersize=3, label=fltr + ' observed', color=dt_clr, rasterized=rasterize)
 
             ax2.errorbar(kwargs['x_data'][fltr], kwargs['residuals'][fltr], yerr=kwargs['y_err'][fltr],
-                         linestyle='none', markersize=3, label=fltr + ' residual', color=dt_clr)
+                         linestyle='none', markersize=3, label=fltr + ' residual', color=dt_clr, rasterized=rasterize)
 
         ax1.plot(kwargs['synth_phases'], curve, label=fltr + ' synthetic', color=clr)
 