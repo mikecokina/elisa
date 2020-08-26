f673b0dac236feb3e445ce1ce358374b98863a96
fredy402 <mirofedurco@gmail.com>
2020-08-26 17:23:03 +0200

    fix: polar temperature calculated using surface elements in spots

--- src/elisa/base/surface/temperature.py
+++ src/elisa/base/surface/temperature.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import copy
 from ... import umpy as up
 
 
@@ -25,9 +26,17 @@ def calculate_polar_effective_temperature(star_container):
     :param star_container: elisa.base.container.StarContainer;
     :return:
     """
-    return star_container.t_eff * up.power(np.sum(star_container.areas) /
-                                           np.sum(star_container.areas * up.power(
-                                               star_container.potential_gradient_magnitudes /
+    areas = copy(star_container.areas)
+    potential_gradient_magnitudes = star_container.potential_gradient_magnitudes
+    if star_container.has_spots():
+        for idx, spot in star_container.spots.items():
+            areas = up.concatenate((areas, spot.areas), axis=0)
+            potential_gradient_magnitudes = \
+                up.concatenate((potential_gradient_magnitudes, spot.potential_gradient_magnitudes), axis=0)
+
+    return star_container.t_eff * up.power(np.sum(areas) /
+                                           np.sum(areas * up.power(
+                                               potential_gradient_magnitudes /
                                                star_container.polar_potential_gradient_magnitude,
                                                star_container.gravity_darkening)),
                                            0.25)
