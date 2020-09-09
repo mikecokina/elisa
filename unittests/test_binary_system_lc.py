83c78fb1d2b5e816d00ec4803943db5c6cdd02c5
miro <mirofedurco@gmail.com>
2020-09-10 00:36:21 +0200

    utest fix no 3

--- unittests/test_binary_system_lc.py
+++ unittests/test_binary_system_lc.py
@@ -441,10 +441,6 @@ class ComputeLightCurvesTestCase(ElisaTestCase):
         self.do_comparison(bs, "detached.ecc.sync.generic.bessell.v.appx_three.json", TOL, -0.0, 0.01, 0.002)
 
     def test_eccentric_asynchronous_detached_system(self):
-        reload(lc)
-        reload(curve_approx)
-        reload(config)
-
         bs = prepare_binary_system(PARAMS["detached-async-ecc"])
 
         self.do_comparison(bs, "detached.ecc.async.generic.bessel.v.json", TOL, -0.2, 1.2, 0.1)
