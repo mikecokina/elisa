horizon computation analysis; fix: <binary_instance>.init() reinitialize stars, spots and discretization factor

unittests/test_binary_system_faces.py
@@ -4,51 +4,53 @@ from numpy.testing import assert_array_equal
 from elisa import umpy as up
 from elisa.binary_system.container import OrbitalPositionContainer
 from elisa.utils import is_empty
+from elisa import units as u
 from unittests import utils as testutils
 from unittests.utils import ElisaTestCase
 
 
 class BuildFacesSpotsFreeTestCase(ElisaTestCase):
     @staticmethod
-    def build_system(key, d):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS[key])
-        s.primary.discretization_factor = d
-        s.secondary.discretization_factor = d
+    def build_system(key):
+        params = testutils.BINARY_SYSTEM_PARAMS[key].copy()
+        params.update({"primary_discretization_factor": 10})
+        s = testutils.prepare_binary_system(params)
+        s.secondary.discretization_factor = 10 * u.deg
+        s.init()
 
         orbital_position_container = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
         orbital_position_container.build_faces(components_distance=1.0)
         return orbital_position_container
 
-    def generator_test_faces(self, key, d, length):
-        orbital_position_container = self.build_system(key, d)
-
+    def generator_test_faces(self, key, length):
+        orbital_position_container = self.build_system(key)
         assert_array_equal([len(orbital_position_container.primary.faces),
                             len(orbital_position_container.secondary.faces)], length)
 
     def test_build_faces_detached(self):
-        self.generator_test_faces('detached', up.radians(10), [848, 848])
+        self.generator_test_faces('detached', [848, 848])
 
     def test_build_faces_over_contact(self):
-        self.generator_test_faces('over-contact', up.radians(10), [812, 784])
+        self.generator_test_faces('over-contact', [812, 784])
 
     def test_build_faces_semi_detached(self):
-        self.generator_test_faces('semi-detached', up.radians(10), [848, 848])
+        self.generator_test_faces('semi-detached', [848, 848])
 
     def test_closed_surface_detached(self):
-        orbital_position_container = self.build_system('detached', up.radians(10))
+        orbital_position_container = self.build_system('detached')
         self.assertTrue(testutils.surface_closed(faces=orbital_position_container.primary.faces,
                                                  points=orbital_position_container.primary.points))
 
     def test_closed_surface_semi_detached(self):
-        orbital_position_container = self.build_system('semi-detached', up.radians(10))
+        orbital_position_container = self.build_system('semi-detached')
         self.assertTrue(testutils.surface_closed(faces=orbital_position_container.primary.faces,
                                                  points=orbital_position_container.primary.points))
 
     def test_closed_surface_over_contact(self):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS['over-contact'])
-        s.primary.discretization_factor = up.radians(10)
-        s.init()
+        params = testutils.BINARY_SYSTEM_PARAMS['over-contact'].copy()
+        params.update({"primary_discretization_factor": 10})
+        s = testutils.prepare_binary_system(params)
 
         orbital_position_container = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
@@ -64,12 +66,12 @@ class BuildFacesSpotsFreeTestCase(ElisaTestCase):
 
 class BuildSpottyFacesTestCase(ElisaTestCase):
     @staticmethod
-    def build_system(key, d):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS[key],
+    def build_system(key):
+        params = testutils.BINARY_SYSTEM_PARAMS[key].copy()
+        params.update({"primary_discretization_factor": 10})
+        s = testutils.prepare_binary_system(params,
                                             spots_primary=testutils.SPOTS_META["primary"],
                                             spots_secondary=testutils.SPOTS_META["secondary"])
-        s.primary.discretization_factor = d
-        s.init()
 
         orbital_position_container = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
@@ -87,12 +89,13 @@ class BuildSpottyFacesTestCase(ElisaTestCase):
         return orbital_position_container
 
     @staticmethod
-    def generator_test_faces(key, d, length):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS[key],
+    def generator_test_faces(key, length):
+        params = testutils.BINARY_SYSTEM_PARAMS[key].copy()
+        params.update({"primary_discretization_factor": 10})
+        s = testutils.prepare_binary_system(params,
                                             spots_primary=testutils.SPOTS_META["primary"],
                                             spots_secondary=testutils.SPOTS_META["secondary"])
-        s.primary.discretization_factor = d
-        s.init()
+
         orbital_position_container = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
         orbital_position_container.build_faces(components_distance=1.0)
@@ -103,31 +106,30 @@ class BuildSpottyFacesTestCase(ElisaTestCase):
                             len(orbital_position_container.secondary.spots[0].faces)], length)
 
     def test_build_faces_detached(self):
-        self.generator_test_faces('detached', up.radians(10), [785, 186, 97, 6])
+        self.generator_test_faces('detached', [785, 186, 97, 6])
 
     def test_build_faces_over_contact(self):
-        self.generator_test_faces('over-contact', up.radians(10), [751, 374, 97, 24])
+        self.generator_test_faces('over-contact', [751, 374, 97, 24])
 
     def test_build_faces_semi_detached(self):
-        self.generator_test_faces('semi-detached', up.radians(10), [785, 400, 97, 24])
+        self.generator_test_faces('semi-detached', [785, 400, 97, 24])
 
     def test_closed_surface_detached(self):
-        orbital_position_container = self.build_system('detached', up.radians(10))
+        orbital_position_container = self.build_system('detached')
         self.assertTrue(testutils.surface_closed(faces=orbital_position_container.primary.faces,
                                                  points=orbital_position_container.primary.points))
 
     def test_closed_surface_semi_detached(self):
-        orbital_position_container = self.build_system('semi-detached', up.radians(10))
+        orbital_position_container = self.build_system('semi-detached')
         self.assertTrue(testutils.surface_closed(faces=orbital_position_container.primary.faces,
                                                  points=orbital_position_container.primary.points))
 
     def test_closed_surface_over_contact(self):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS['over-contact'],
+        params = testutils.BINARY_SYSTEM_PARAMS['over-contact'].copy()
+        params.update({"primary_discretization_factor": 7})
+        s = testutils.prepare_binary_system(params,
                                             spots_primary=testutils.SPOTS_META["primary"],
                                             spots_secondary=testutils.SPOTS_META["secondary"])
-        s.primary.discretization_factor = up.radians(7)
-        s.init()
-
         orbital_position_container = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
         orbital_position_container.build_faces(components_distance=1.0)
@@ -151,12 +153,12 @@ class BuildSpottyFacesTestCase(ElisaTestCase):
 
 
 class BuildSurfaceAreasTestCase(ElisaTestCase):
-    def generator_test_surface_areas(self, key, d, kind, less=None):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS[key],
+    def generator_test_surface_areas(self, key, kind, less=None):
+        params = testutils.BINARY_SYSTEM_PARAMS[key].copy()
+        params.update({"primary_discretization_factor": 10})
+        s = testutils.prepare_binary_system(params,
                                             spots_primary=testutils.SPOTS_META["primary"],
                                             spots_secondary=testutils.SPOTS_META["secondary"])
-        s.primary.discretization_factor = d
-        s.init()
         orbital_position_container = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
         orbital_position_container.build_faces(components_distance=1.0)
@@ -177,31 +179,31 @@ class BuildSurfaceAreasTestCase(ElisaTestCase):
             self.assertTrue(np.all(up.less(orbital_position_container.secondary.spots[0].areas, less)))
 
     def test_build_surface_areas_detached(self):
-        self.generator_test_surface_areas('detached', up.radians(10), kind="contain")
+        self.generator_test_surface_areas('detached', kind="contain")
 
     def test_build_surface_areas_over_contact(self):
-        self.generator_test_surface_areas('over-contact', up.radians(10), kind="contain")
+        self.generator_test_surface_areas('over-contact', kind="contain")
 
     def test_build_surface_areas_semi_detached(self):
-        self.generator_test_surface_areas('semi-detached', up.radians(10), kind="contain")
+        self.generator_test_surface_areas('semi-detached', kind="contain")
 
     def test_build_surface_areas_detached_size(self):
-        self.generator_test_surface_areas('detached', up.radians(10), kind="size", less=5e-6)
+        self.generator_test_surface_areas('detached', kind="size", less=5e-6)
 
     def test_build_surface_areas_over_contact_size(self):
-        self.generator_test_surface_areas('over-contact', up.radians(10), kind="size", less=8e-3)
+        self.generator_test_surface_areas('over-contact', kind="size", less=8e-3)
 
     def test_build_surface_areas_semi_detached_size(self):
-        self.generator_test_surface_areas('semi-detached', up.radians(10), kind="size", less=8e-3)
+        self.generator_test_surface_areas('semi-detached', kind="size", less=8e-3)
 
 
 class BuildSpottyFacesOrientationTestCase(ElisaTestCase):
     def generator_test_face_orientaion(self, key, kind):
-        s = testutils.prepare_binary_system(testutils.BINARY_SYSTEM_PARAMS[key],
+        params = testutils.BINARY_SYSTEM_PARAMS[key].copy()
+        params.update({"primary_discretization_factor": 7})
+        s = testutils.prepare_binary_system(params,
                                             spots_primary=testutils.SPOTS_META["primary"],
                                             spots_secondary=testutils.SPOTS_META["secondary"])
-        s.primary.discretization_factor = up.radians(7)
-        s.init()
         orbital_position_container: OrbitalPositionContainer = testutils.prepare_orbital_position_container(s)
         orbital_position_container.build_mesh(components_distance=1.0)
         orbital_position_container.build_faces(components_distance=1.0)