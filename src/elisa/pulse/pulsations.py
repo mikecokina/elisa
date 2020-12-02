ec4fa7b2e7f1adc3e4a3259348e84460c2752cb9
fredy402 <mirofedurco@gmail.com>
2020-12-02 15:48:19 +0100

    position perturbation relocated

--- src/elisa/pulse/pulsations.py
+++ src/elisa/pulse/pulsations.py
@@ -5,6 +5,7 @@ from .. umpy import sph_harm
 from .. import settings
 from .. logger import getLogger
 from . import utils as putils
+from . surface.position import calculate_mode_displacement
 
 logger = getLogger('pulse.pulsations')
 
@@ -75,6 +76,7 @@ def incorporate_pulsations_to_mesh(star_container, com_x, scale=1.0):
     for mode_index, mode in star_container.pulsations.items():
         displacement += calculate_mode_displacement(mode, tilted_points, mode.point_harmonics,
                                                     mode.point_harmonics_derivatives, scale=scale)
+        # velocity_displacement =
         for spot_idx, spoints in tilted_points_spot.items():
             displacement_spots[spot_idx] += \
                 calculate_mode_displacement(mode, spoints, mode.spot_point_harmonics[spot_idx],
@@ -166,67 +168,6 @@ def assign_amplitudes(star_container, normalization_constant=1.0):
                            f' {mode_index} exceeded safe tolerances ({settings.SURFACE_DISPLACEMENT_TOL}) given by the'
                            f' use of linear approximation. This can lead to invalid surface discretization. Use this'
                            f' result with caution.')
-
-
-def calculate_radial_displacement(mode, harmonics):
-    """
-    Calculates radial displacement of surface points.
-
-    :param mode: PulsationMode;
-    :param harmonics: numpy.array; Y_l^m
-    :return: numpy.array;
-    """
-    return mode.radial_amplitude * np.real(harmonics)
-
-
-def calculate_phi_displacement(mode, thetas, harmonics_derivatives):
-    """
-    Displacement of azimuthal coordinates.
-
-    :param mode: PulsationMode;
-    :param thetas: numpy.array
-    :param harmonics_derivatives: numpy.array; dY/dphi
-    :return: numpy.array;
-    """
-    sin2_thetas = np.power(np.sin(thetas), 2)
-    sin_test = sin2_thetas != 0.0
-    retval = np.zeros(thetas.shape)
-    retval[sin_test] = \
-        mode.horizontal_amplitude * np.real(harmonics_derivatives[sin_test]) \
-        / sin2_thetas[sin_test]
-    return retval
-
-
-def calculate_theta_displacement(mode, harmonics_derivatives):
-    """
-    Displacement in latitude.
-
-    :param harmonics_derivatives: numpy.array; dY/dtheta
-    :param mode: PulsationMode;
-    :return: numpy.array;
-    """
-    return mode.horizontal_amplitude * np.real(harmonics_derivatives)
-
-
-def calculate_mode_displacement(mode, points, harmonics, harmonics_derivatives, scale=1.0):
-    """
-    Calculates surface displacement caused by given `mode`.
-
-    :param mode: elisa.pulse.mode.Mode;
-    :param points: numpy.array; in spherical coordinates
-    :param harmonics: numpy.array; Y_l^m
-    :param harmonics_derivatives: numpy.array; [dY/dphi, dY/dtheta]
-    :param scale: numpy.float; scale of the perturbations
-    :return: numpy.array;
-    """
-    if settings.PULSATION_MODEL == 'uniform':
-        radial_displacement = calculate_radial_displacement(mode, harmonics) / scale
-        phi_displacement = calculate_phi_displacement(mode, points[:, 2], harmonics_derivatives[0])
-        theta_displacement = calculate_theta_displacement(mode, harmonics_derivatives[1])
-
-        return np.column_stack((radial_displacement, phi_displacement, theta_displacement))
-    else:
-        raise NotImplementedError(f'Pulsation model: {settings.PULSATION_MODEL} not implemented.')
 
 
 def calculate_acc_pert(mode, points, harmonics, harmonics_derivatives):
