f973455a996e81b0387df9fd81bd0ac1a9216cdb
miro <mirofedurco@gmail.com>
2021-03-02 12:30:12 +0100

    utest fixes

--- unittests/utils.py
+++ unittests/utils.py
@@ -462,7 +462,7 @@ APPROX_SETTINGS = \
     {"no_approx":
          {"MAX_RELATIVE_D_R_POINT": 0.0, "MAX_NU_SEPARATION": 0.08},
      "approx_one":
-         {"MAX_NU_SEPARATION": 1.0, "MIN_POINTS_IN_ECLIPSE": 10},
+         {"MAX_NU_SEPARATION": 1.0, "MIN_POINTS_IN_ECLIPSE": 30},
      "approx_two":
          {"MAX_NU_SEPARATION": 0.00, "MAX_RELATIVE_D_R_POINT": 0.001},
      "approx_three":
