58ccb752ccd2614351c73bd7bbe82c6897ec8a75
miro <mirofedurco@gmail.com>
2021-09-23 15:56:39 +0200

    analytics.models.rv

--- src/elisa/analytics/models/lc.py
+++ src/elisa/analytics/models/lc.py
@@ -8,7 +8,7 @@ from .. models.serializers import (
 
 def prepare_binary(discretization=5, _verify=False, **kwargs):
     """
-    Setup binary system from initial fit parameter JSON.
+    Setup binary system from initial parameter object.
     If `metallicity` is not supplied 0 is used to initialize the parameter. Similarly, default value of `synchronicity`
     parameter is 1.0.
 
@@ -31,12 +31,16 @@ def prepare_binary(discretization=5, _verify=False, **kwargs):
 
 def synthetic_binary(phases, discretization, observer, **kwargs):
     """
-    Function returns synthetic light curve of binary system based on a set of model parameter.
+    Function returns synthetic light curve of binary system based on a set of model parameters.
 
-    :param phases: Union[List, numpy.array]; photometric phaeses in ehich the curves will be generated
+    :param phases: Union[List, numpy.array]; photometric phases in which the curves will be generated
     :param discretization: float; primary component's surface discretization factor
     :param observer: elisa.observer.observer.Observer; observer instance
-    :param kwargs: Dict; complete set of model parameters in flat format {'parameter@name': value, }
+    :param kwargs: Dict; The structure of the `kwargs` is similar to the input JSON used in BinarySystem.from_json()
+                         function, with exception of using the flat format {'parameter@name': value, } instead of the
+                         nested structure. The default units for parameters are the default input units
+                         (see elisa.units).
+
     :return: Dict[str, numpy.array]; light curves in in format {'passband_name': LC in filter}
     """
     binary = prepare_binary(discretization, **kwargs)
--- src/elisa/analytics/models/rv.py
+++ src/elisa/analytics/models/rv.py
@@ -6,23 +6,34 @@ from ... binary_system.system import BinarySystem
 from ... binary_system.utils import resolve_json_kind
 
 
-def central_rv_synthetic(x_data, observer, **kwargs):
+def prepare_central_rv_binary(**kwargs):
     """
+    Setup binary system from initial parameter object.
 
-    :param x_data: Union[List, numpy.array];
-    :param observer: elisa.observer.observer.Observer; instance
+    :param kwargs: Dict; complete set of model parameters in flat format {'parameter@name': value, }
+    :return: elisa.binary_system.system.BinarySystem
+    """
+    return BinarySystem.from_json(kwargs, _verify=False)
+
+
+def central_rv_synthetic(phases, observer, **kwargs):
+    """
+    Function returns synthetic RV curve of binary system based on a set of model parameters.
+
+    :param phases: Union[List, numpy.array]; photometric phases in which the curves will be generated
+    :param observer: elisa.observer.observer.Observer; observer instance
     :param kwargs: Dict;
     :**kwargs options**:
-        * **system@argument_of_periastron** * -- float;
-        * **system@eccentricity** * -- float;
-        * **system@inclination** * -- float;
-        * **primary@mass** * -- float;
-        * **secondary@mass** * -- float;
-        * **system@gamma** * -- float;
-        * **system@asini** * --float;
-        * **system@mass_ratio** * --float;
-        * **system@primary_minimum_time** * -- float;
-        * **system@period** * -- float;
+        * **'system@argument_of_periastron'**: float;
+        * **'system@eccentricity'**: float;
+        * **'system@inclination'**: float;
+        * **'primary@mass'**: float;
+        * **'secondary@mass'**: float;
+        * **'system@gamma'**: float;
+        * **'system@asini'**: float;
+        * **'system@mass_ratio'**: float;
+        * **'system@primary_minimum_time'**: float;
+        * **'system@period'**: float;
 
     :return: Tuple;
     """
@@ -36,7 +47,7 @@ def central_rv_synthetic(x_data, observer, **kwargs):
         "secondary@metallicity": 10000.0,
     })
 
-    x_data_resolved, kwargs = time_layer_resolver(x_data, pop=True, **kwargs)
+    x_data_resolved, kwargs = time_layer_resolver(phases, pop=True, **kwargs)
 
     system_kwargs = serializers.serialize_system_kwargs(**kwargs)
     primary_kwargs = serializers.serialize_primary_kwargs(**kwargs)
@@ -62,7 +73,3 @@ def central_rv_synthetic(x_data, observer, **kwargs):
     _, rv = observer.observe.rv(phases=x_data_resolved, normalize=False)
 
     return rv
-
-
-def prepare_central_rv_binary(**kwargs):
-    return BinarySystem.from_json(kwargs, _verify=False)
