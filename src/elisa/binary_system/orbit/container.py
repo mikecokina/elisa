677fd7f9f238dc7e6902af9eca10f89c73a58bb9
miro <mirofedurco@gmail.com>
2021-09-28 15:31:42 +0200

    binary_system.orbit

--- src/elisa/binary_system/orbit/container.py
+++ src/elisa/binary_system/orbit/container.py
@@ -12,7 +12,15 @@ logger = getLogger("orbit.container")
 
 class OrbitalSupplements(Sequence):
     """
+    Structure designed to store orbital positions on (nearly) symmetrical orbital positions around apsidal line.
+    The symmetrical counterparts are stored in the same positions within 'body' and 'mirror' position arrays.
+    Corresponding `body` and `mirror` items share the same bindary system model which saves computational time.
     !!! BEWARE, THIS IS MUTABLE !!!
+
+    :param body: numpy.array; N x 5 array containing row-wise orbital positions used as templates
+                             (they will be evaluated exactly)
+    :param mirror: numpy.array; N x 5 array containing row-wise orbital positions mirrored from corresponding
+                               `body` orbital positions using symmetry along apsidal line.
     """
 
     def __getitem__(self, index):
@@ -28,27 +36,38 @@ class OrbitalSupplements(Sequence):
             self._mirror = np.array(mirror)
 
     def append(self, body, mirror):
+        """
+        Appending corresponding `body`/`mirror` couple to the end of their respective arrays.
+
+        :param body: numpy.array; [index, ] + [items for items in elisa.const.Position]
+        :param mirror: numpy.array; [index, ] + [items for items in elisa.const.Position]
+        """
         self._body = np.vstack((self._body, body)) if not is_empty(self._body) else np.array([body])
         self._mirror = np.vstack((self._mirror, mirror)) if not is_empty(self._mirror) else np.array([mirror])
 
     @property
     def body(self):
+        """Returns an array of body positions."""
         return self._body
 
     @property
     def mirror(self):
+        """Returns an array of mirror positions."""
         return self._mirror
 
     @property
     def body_defined(self):
+        """Returns True if `body` position array is not empty."""
         return self.not_empty(self.body)
 
     @property
     def mirror_defined(self):
+        """Returns True if `mirror` position array is not empty."""
         return self.not_empty(self.mirror)
 
     @staticmethod
     def is_empty(val):
+        """Return True if given orbital position is empty."""
         return np.all(up.isnan(val))
 
     @classmethod
@@ -67,8 +86,9 @@ class OrbitalSupplements(Sequence):
         This method sorts bodies and mirrors based on quantity chosen on input.
         Sorting of mirrors is based on sorting of bodies.
 
-        :param by: str;
-        :return: self;
+        :param by: str; `index`, `distance` or `radius` sorting orbital positions in `body` an `mirror` according to
+                         the index of `body positions or by component distances.
+        :return: self; sorted OrbitalSupplements object
         """
 
         if by == 'index':
@@ -91,23 +111,32 @@ class OrbitalSupplements(Sequence):
         pass
 
     def plot_bodies(self):
+        """
+        Visualize orbital positions stored in `body` .
+        """
         self._plot(self.body_defined)
 
     def plot_mirrors(self):
-        self._plot(self.mirror_defined, marker="x")
+        """
+        Visualize orbital positions stored in `mirror` .
+        """
+        self._plot(self.mirror_defined, markers=["x", "x"])
 
     def plot(self):
+        """
+        Visualize orbital positions stored in OrbitalSupplements.
+        """
         self._plot(self.body_defined, self.mirror_defined)
 
     @classmethod
-    def _plot(cls, arr1, arr2=None, marker="o"):
-
+    def _plot(cls, arr1, arr2=None, markers=None):
+        markers = ["o", "x"] if markers is None else markers
         x, y = polar_to_cartesian(arr1[:, 1], arr1[:, 2] - (up.pi / 2))
-        plt.scatter(x, y, marker=marker)
+        plt.scatter(x, y, marker=markers[0])
 
         if not is_empty(arr2):
             x, y = polar_to_cartesian(arr2[:, 1], arr2[:, 2] - (up.pi / 2))
-            plt.scatter(x, y, marker="x")
+            plt.scatter(x, y, marker=markers[1])
 
         plt.grid(True)
         plt.axes().set_aspect('equal')
